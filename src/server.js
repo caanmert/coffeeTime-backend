a751e7e87a223305e905e601a32d0b90c3e6137d
Can Mert <canmert.dev@gmail.com>
2021-10-13 11:36:54 +0300

    adding packages and eslint

---
+++ src/server.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+const express = require('express')
+const cors = require('cors');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const helmet = require('helmet');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+const port = process.env.PORT || 5000;
+app.use(cors());
+app.post('*', cors());
+app.options('*', cors());
+app.use(express.json({ limit: '40mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+app.use(cookieParser());
+app.use(helmet());
+app.use('*', (req, res, next) => {
+  res.setHeader('Content-Type', 'application/json');
+  next();
+});
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('*', (req, res) => {
+    res.status(404).json({ status: false, message: 'Address not found' });
+  });
+  
+  app.listen(port, () => {
+    console.log(`Listening on port ${port}!`);
+  });
+  
